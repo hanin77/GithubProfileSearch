b6f04995ae24eb03483c465bf124cba3bac104d6
hanin77 <mohammadhanin77@gmail.com>
2020-07-04 04:47:47 +0100

    added GitApi

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,49 @@
 import React, {Component, Fragement} from 'react';
 import Users from './Users';
-import {Button, FormControl, Form, FormGroup} from 'react-bootstrap';
+import axios from 'axios';
+import {Button, Spinner, FormData, FormControl, Form, FormGroup} from 'react-bootstrap';
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            textSearch: ''
+            users: [],
+            textSearch: 'hanin',
+            searchOrNot: false,
+            loading: false
         }
     }
 
     onTextSearchChange = (e) => {
         const textSearch = e.target.value;
         this.setState(() => ({textSearch}));
+        
     }
 
     onSubmitForm = (e) => {
         e.preventDefault();
-        console.log('ok');
-        this.setState(() => ({textSearch: ''}));
+        console.log("ok");
+        this.setState((prevState) => ({searchOrNot: true, loading: true}));
+    }
+
+
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('nextstate',nextState.textSearch);
+        console.log('curentstate',this.state.textSearch);
+        return(this.state.searchOrNot);
+    }
+   
+    componentDidUpdate() { 
+        axios.get(`https://api.github.com/search/users?q=${this.state.textSearch}&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET}`).then((result)=> {
+            console.log(result.data.items);
+            this.setState(() => (
+                {
+                 users:  [...result.data.items],
+                 searchOrNot: false,
+                 loading: false
+                }
+            ));
+        });
     }
 
     render() {
@@ -25,11 +51,11 @@ class Home extends React.Component {
             <div>
                 <Form onSubmit = {this.onSubmitForm} >
                     <FormGroup role='form' className="mx-3">
-                        <FormControl required className="form-control my-2" type='text' placeholder='Search for a User' onChange={this.onTextSearchChange}></FormControl>
+                        <FormControl required className="form-control my-2" name='textSearch' type='text' placeholder='Search for a User' onChange={this.onTextSearchChange}></FormControl>
                         <Button className='btn btn-success btn-block' type="submit">Search</Button>
                     </FormGroup>
                 </Form>
-                <Users />
+                {this.state.loading? <div className='d-flex justify-content-center'><Spinner className='p-2' animation="border" /></div>: <Users users={this.state.users} />}
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -43,12 +43,8 @@ module.exports = (env) => {
         filename: 'styles.css'
       }),
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+        'process.env.REACT_APP_GITHUB_ID': JSON.stringify(process.env.REACT_APP_GITHUB_ID),
+        'process.env.REACT_APP_GITHUB_SECRET': JSON.stringify(process.env.REACT_APP_GITHUB_SECRET)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
